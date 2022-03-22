05e41fd9be7841002a2273b0c6028898d82b4f02
Bruno Pereira <brunoalmeidapereira@gmail.com>
2022-03-21 21:10:55 -0300

    Feat: Create roles seeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = Role::create(['name' => 'Admin']);
+        $role = Role::create(['name' => 'operador']);
+        $role = Role::create(['name' => 'usuario']);
+    }
+}
