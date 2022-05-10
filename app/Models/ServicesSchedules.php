d2c0119e76a35cbaaf21615a2c106301460acc6a
Bruno Pereira <brunoalmeidapereira@gmail.com>
2022-05-10 20:09:14 -0300

    feat:create serviceSchedules model

---
+++ app/Models/ServicesSchedules.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ServicesSchedules extends Model
+{
+    use HasFactory;
+
+    protected $table = 'services_schedules';
+
+
+}
