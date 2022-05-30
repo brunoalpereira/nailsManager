5c7a4523ed14e1a6eb4675c3ef35a04e3a2e0c3a
Bruno Pereira <brunoalmeidapereira@gmail.com>
2022-05-30 20:08:10 -0300

    Refactor:change contrunction of sideBar

--- resources/views/layouts/sideBar.blade.php
+++ resources/views/layouts/sideBar.blade.php
@@ -1,38 +1,41 @@
 <body>
+
     <div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
         <div class="nano">
             <div class="nano-content">
-                <ul>
-                    <div class="logo"><a href="index.html">
-                        <i class="fas fa-paint-brush me-2"></i>
-                        <span>Nails Manager</span>
-                    </div>
-                  
-                    <li class="label"></li>
-                    <li><a href="#">
-                    <i class="far fa-bookmark"></i> Atendimentos </a></li>
+                    <ul>
+                        <div class="logo"><a href="index.html">
+                            <i class="fas fa-paint-brush me-2"></i>
+                            <span>Nails Manager</span>
+                        </div>
+                    
+                        <li class="label"></li>
+                        <li><a href="#">
+                        <i class="far fa-bookmark"></i> Atendimentos </a></li>
 
-                    <li class="label"></li>
-                    <li><a href="#">
-                    <i class="fas fa-address-book"></i> Agendamentos </a></li>
+                        <li class="label"></li>
+                        <li><a href="#">
+                        <i class="fas fa-address-book"></i> Agendamentos </a></li>
 
-                    <li class="label"></li>
-                    <li><a href="#">
-                    <i class="far fa-user"></i> Usuários </a></li>
+                        <li class="label"></li>
+                        <li><a href="#">
+                        <i class="far fa-user"></i> Usuários </a></li>
 
-                    <li class="label"></li>
-                    <li><a href="#">
-                    <i class="fas fa-stream"></i> Serviços </a></li>
+                        <li class="label"></li>
+                        <li><a href="#">
+                        <i class="fas fa-stream"></i> Serviços </a></li>
 
-                    <li class="label"></li>
-                    <li><a href="#">
-                    <i class="fas fa-file-alt"></i> Relatórios </a></li>
+                        <li class="label"></li>
+                        <li><a href="#">
+                        <i class="fas fa-file-alt"></i> Relatórios </a></li>
 
-                    <li class="label"></li>
-                    <li><a href="#">
-                    <i class="fas fa-chart-line"></i> Gráficos </a></li>
-                  
-            </div>
-        </div>
+                        <li class="label"></li>
+                        <li><a href="#">
+                        <i class="fas fa-chart-line"></i> Gráficos </a></li>
+                    </ul>
+
+            </div>            
+        </div>  
     </div>
+    <!-- /# sidebar -->
 </body>
