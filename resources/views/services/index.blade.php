dded0581bcf3c4df1af0cdc03de2055a225b0353
Bruno Pereira <brunoalmeidapereira@gmail.com>
2022-06-27 19:50:37 -0300

    Feat: rework in layout

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -25,40 +25,44 @@
                     </a>
                 </div>     
                 @foreach($services as $service)
-                <div class="card mt-2">
-                    <div class="card-body">
-                        <div class="d-flex flex-column flex-md-row justify-content-between">
-                            <a class="card-title" href="/services/edit/{{ $service->id }}">{{ $service->name }}</a>
-                            <h5 class="card-subtitle">Valor
-                            <span class="card-text">{{ 'R$'. $service->value}}</span>
-                            </h5> 
+                <div class=" col-lg-12">
+                    <div class="card mt-2">
+                        <div class="card-body">
+                            <div class="d-flex flex-column flex-md-row justify-content-between">
 
-                        
-                            <!-- <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>  -->
-                           
-                            <div class="d-grid  d-md-flex justify-content-md-end">
+                                <a class="card-title col-lg-2" href="/services/edit/{{ $service->id }}">{{ $service->name }}</a>
+                                <label class="card-text col-lg-2" >Valor
+                                <h6 >{{ 'R$ : '. $service->value}}                    
+                                </h6> 
+                                </label>
+                                <!-- <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>  -->
+                            
+                                <div class="d-grid  d-lg-flex justify-content-lg-end">
+                                    <form>
+                                        <a class="btn btn-secondary  mx-2" href="/services/edit/{{ $service->id }}">
+                                            <span class="icon text-white-50">
+                                            <i class="fas fa-edit"></i>
+                                            </span>
+                                            <span class="text">Editar</span>
+                                        </a>
+                                    </form>
+                                    <form action="/services/delete/{{ $service->id }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                        <button type="submit" class="btn btn-danger delete-btn">
+                                            <span class="icon text-white-50">
+                                            <i class="fas fa-trash"></i>
+                                            </span>
+                                            <span class="text">Excluir</span></button>
+                                    </form>
 
-                            <a class="btn btn-secondary mx-2" href="/services/edit/{{ $service->id }}">
-                                    <span class="icon text-white-50">
-                                    <i class="fas fa-edit"></i>
-                                    </span>
-                                    <span class="text">Editar</span>
-                                </a>
-
-                                <form action="/services/delete/{{ $service->id }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                                    <button type="submit" class="btn btn-danger delete-btn"><ion-icon name="trash-outline"></ion-icon>
-                                        <span class="icon text-white-50">
-                                        <i class="fas fa-trash"></i>
-                                        </span>
-                                        <span class="text">Excluir</span></button>
-                                </form>
-                            </div>                            
+                                    
+                                </div>                            
+                            </div>
                         </div>
+                            
                     </div>
-                           
-                </div>
+                 </div>   
                 @endforeach
             </div>
         </div>
