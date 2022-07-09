d994e5364a62a7f837384c8b826a711e2c9a825b
Bruno Pereira <brunoalmeidapereira@gmail.com>
2022-07-09 11:41:42 -0300

    Feat: add personal infos in side bar

--- resources/views/layouts/sideBar.blade.php
+++ resources/views/layouts/sideBar.blade.php
@@ -1,74 +1,83 @@
-
 <!-- sideBar-->
-    <div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
-        <div class="nano">
-            <div class="nano-content">
-                    <ul>
-                        <div class="logo"><a href="index.html">
-                            <i class="fas fa-paint-brush me-2"></i>
-                            <span>Nails Manager</span>
-                        </div>
-                    
-                        <li class="label"></li>
-                        <li><a href="#">
+<div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
+    <div class="nano">
+        <div class="nano-content">
+            <ul>
+                <div class="logo"><a href="index.html">
+                        <i class="fas fa-paint-brush me-2"></i>
+                        <span>Nails Manager</span>
+                </div>
+
+                <li class="label"></li>
+                <li><a href="#">
                         <i class="far fa-bookmark"></i> Atendimentos </a></li>
 
-                        <li class="label"></li>
-                        <li><a href="#">
+                <li class="label"></li>
+                <li><a href="#">
                         <i class="fas fa-address-book"></i> Agendamentos </a></li>
 
-                        <li class="label"></li>
-                        <li><a href="#">
-                        <i class="far fa-user"></i> Usuários </a></li>
+                <li><a class="sidebar-sub-toggle">
+                        <i class="far fa-user">
+                        </i> Usuários
+                        <span class="sidebar-collapse-icon ti-angle-down">
+                        </span>
+                    </a>
 
-                        <li class="label"></li>
-                        <li><a href="/services">
+                    <ul>
+                        <li><a href="/personal-infos"><i class="far fa-address-card"></i>Cadastro Informações</a></li>
+                    </ul>
+                </li>
+
+
+                <li class="label"></li>
+                <li><a href="/services">
                         <i class="fas fa-stream"></i> Serviços </a></li>
 
-                        <li class="label"></li>
-                        <li><a href="#">
+                <li class="label"></li>
+                <li><a href="#">
                         <i class="fas fa-file-alt"></i> Relatórios </a></li>
 
-                        <li class="label"></li>
-                        <li><a href="#">
+                <li class="label"></li>
+                <li><a href="#">
                         <i class="fas fa-chart-line"></i> Gráficos </a></li>
-                    </ul>
 
-            </div>            
-        </div>  
+
+            </ul>
+
+        </div>
     </div>
-    <!-- /# sidebar -->
+</div>
+<!-- /# sidebar -->
 
-    <!-- Nav Menu -->
-    <div class="header">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="float-left">
-                        <div class="hamburger sidebar-toggle">
-                            <span class="line"></span>
-                            <span class="line"></span>
-                            <span class="line"></span>
-                        </div>
+<!-- Nav Menu -->
+<div class="header">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="float-left">
+                    <div class="hamburger sidebar-toggle">
+                        <span class="line"></span>
+                        <span class="line"></span>
+                        <span class="line"></span>
                     </div>
-                    <div class="float-right">
-                       
-                        <div class="dropdown dib">
-                            <div class="header-icon" data-toggle="dropdown">
-                                <span class="user-avatar">
-                                    <i class="ti-angle-down f-s-10"></i>
-                                </span>
-                                <div class="drop-down dropdown-profile dropdown-menu dropdown-menu-right">
-                                    <div class="dropdown-content-body">
-                                        <ul>
-                                            <li>
-                                                <a href="#">
-                                                    <i class="ti-power-off"></i>
-                                                    <span>Sair</span>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
+                </div>
+                <div class="float-right">
+
+                    <div class="dropdown dib">
+                        <div class="header-icon" data-toggle="dropdown">
+                            <span class="user-avatar">
+                                <i class="ti-angle-down f-s-10"></i>
+                            </span>
+                            <div class="drop-down dropdown-profile dropdown-menu dropdown-menu-right">
+                                <div class="dropdown-content-body">
+                                    <ul>
+                                        <li>
+                                            <a href="#">
+                                                <i class="ti-power-off"></i>
+                                                <span>Sair</span>
+                                            </a>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
@@ -77,4 +86,5 @@
             </div>
         </div>
     </div>
-    <!-- /# navMenu -->
+</div>
+<!-- /# navMenu -->
