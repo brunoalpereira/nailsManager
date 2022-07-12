0c772f1873ce0747c948bc8ee0d945b56fc4c57b
Bruno Pereira <brunoalmeidapereira@gmail.com>
2022-07-12 20:53:46 -0300

    Feat:  store function in personal infos controller

--- app/Http/Controllers/PersonalInfosController.php
+++ app/Http/Controllers/PersonalInfosController.php
@@ -3,16 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use app\Models\UsersPersonalInfo;
+use App\Models\UsersPersonalInfo;
 
 class PersonalInfosController extends Controller
 {
 
-    public function create() {
+  public function create()
+  {
         return view('personalInfos.create');
     }
 
-    public function index(){
+  public function store(Request $request)
+  {
+
+    $personalInfos = new UsersPersonalInfo();
+
+    $personalInfos->id_user = $request->user;
+    $personalInfos->address = $request->address;
+    $personalInfos->complement = $request->complement;
+    $personalInfos->address_number = $request->address_number;
+    $personalInfos->cep = $request->cep;
+    $personalInfos->phone = $request->phone;
+
+    $personalInfos->save();
+    return redirect('/personal-infos')->with('msg', 'Informações gravado com sucesso!');
+  }
 
       return view('personalInfos.index' );
     }
