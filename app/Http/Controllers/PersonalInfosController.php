fb31800a84e5dbe4bb297c431a933979dd2195a4
Bruno Pereira <brunoalmeidapereira@gmail.com>
2022-08-10 21:18:49 -0300

    Refactor:all functions on personal infoscontroller

--- app/Http/Controllers/PersonalInfosController.php
+++ app/Http/Controllers/PersonalInfosController.php
@@ -2,31 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\UsersPersonalInfo;
+use Illuminate\Support\Facades\DB;
 
 class PersonalInfosController extends Controller
 {
 
   public function create()
   {
-    return view('personalInfos.create');
+     $users = User::all();
+
+    return view('personalInfos.create',['users'=>$users]);
   }
 
   public function edit($id)
   {
 
     $personalInfos = UsersPersonalInfo::findOrFail($id);
+    $users = User::all();
 
-    return view('personalInfos.edit', ['personalInfos' => $personalInfos]);
+    return view('personalInfos.edit', ['personalInfos' => $personalInfos,
+                                      'users'=>$users]);
   }
 
   public function store(Request $request)
   {
 
+    if($request->id_user == 0 ){
+      $user = Auth::id();
+    } else {
+      $user =$request->id_user;
+    }
+
     $personalInfos = new UsersPersonalInfo();
 
-    $personalInfos->id_user = $request->user;
+    $personalInfos->id_user = $user;
     $personalInfos->address = $request->address;
     $personalInfos->complement = $request->complement;
     $personalInfos->address_number = $request->address_number;
@@ -42,7 +55,13 @@ class PersonalInfosController extends Controller
 
     $personalInfos = UsersPersonalInfo::where('id', $id)->first();
 
-    $personalInfos->id_user = $request->user;
+    if($request->id_user == 0 ){
+      $user = Auth::id();
+    } else {
+      $user =$request->id_user;
+    }
+
+    $personalInfos->id_user = $user;
     $personalInfos->address = $request->address;
     $personalInfos->complement = $request->complement;
     $personalInfos->address_number = $request->address_number;
@@ -56,8 +75,38 @@ class PersonalInfosController extends Controller
 
   public function index()
   {
+    $user = Auth::id();
+    $roles = [];
 
-    $personalInfos = UsersPersonalInfo::all();
+    $userRoles = DB::table('users')
+    ->join('model_has_roles','model_has_roles.model_id','=','users.id')
+    ->join('roles','model_has_roles.role_id','=','roles.id')
+    ->select('roles.name')
+    ->where('users.id',$user)
+    ->get();
+
+    foreach($userRoles as $userRole){
+      array_push($roles,$userRole->name);
+    }
+    
+    if(in_array('Admin',$roles) || in_array('operador',$roles)){
+      
+      $personalInfos =DB::table('user_personal_infos')
+      ->join('users','users.id','=','user_personal_infos.id_user')
+      ->select('user_personal_infos.phone',
+              'user_personal_infos.id',
+            'user_personal_infos.address',
+            'users.name')
+            ->where('deleted_at',null)
+      ->get();
+
+
+    } else{
+      $personalInfos = UsersPersonalInfo::where('id_user',$user)
+      ->where('deleted_at',null);
+    }
+
+   
 
     return view(
       'personalInfos.index',
