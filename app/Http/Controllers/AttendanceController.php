6abdbbd3a17401dc4d24985ed86606b4295a096c
Bruno Pereira <brunoalmeidapereira@gmail.com>
2022-09-01 20:57:30 -0300

    Feat: create attendance create fuction

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Request;
 
 class AttendanceController extends Controller
 {
+      public function create()
+      {
+        $services = Services::all();
+  
+        $users = User::all();
+        return view('attendance.create',
+                   ['services'=> $services,
+                   'users'=>$users]);
+      }
     public function index()
     {
   
