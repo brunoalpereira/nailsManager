381d425bb069e119bcdd85544ad9c23142e5422d
Bruno Pereira <brunoalmeidapereira@gmail.com>
2022-09-01 20:59:25 -0300

    Feat: create attendace update fucntio

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -84,3 +84,53 @@ class AttendanceController extends Controller
   
         return redirect('/attendance')->with('msg', 'Informações gravado com sucesso!');
 }
+      public function update(Request $request, $id)
+      {
+  
+        $schedules = Schedules::where('id', $id)->first();
+  
+        $schedules->date = $request->date;
+        $schedules->hour = $request->hour;
+        $schedules->observations = $request->observations;  
+        $schedules->status = 'Finalizado';   
+        $schedules->updated_at= Carbon::now();
+        $schedules->save();
+  
+        $oldServices= DB::table('services_schedules')
+        ->select('services_schedules.id')
+        ->where('services_schedules.schedules_id',$id)
+        ->get()
+        ->toArray();
+  
+  
+        foreach($oldServices as $serv){
+          ServicesSchedules::findOrFail($serv->id)->delete();
+        }
+  
+  
+        $services = $request->services;
+        foreach($services as $service){
+          DB::table('services_schedules')
+          ->insert([
+            'service_id' => $service,
+            'schedules_id'=> $id,
+            'created_at'=> Carbon::now(),
+            'updated_at'=>Carbon::now()
+          ]);
+        }
+
+        $operator = Auth::id();
+
+        DB::table('attendance')
+          ->insert([
+            'schedules_id'=> $id,
+            'operator_id'=> $operator,
+            'is_finished'=>true,
+            'created_at'=> Carbon::now(),
+            'updated_at'=>Carbon::now()
+          ]);
+    
+        
+        return redirect('/attendance')->with('msg', 'Informações editadas com sucesso!');
+      }
+    
