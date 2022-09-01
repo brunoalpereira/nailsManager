5c544e01ffe4634a3166527ffdb90f8d62e3ba46
Bruno Pereira <brunoalmeidapereira@gmail.com>
2022-09-01 20:58:55 -0300

    Feat: create a store function to attendance

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -38,4 +38,49 @@ class AttendanceController extends Controller
         ['schedules' => $schedules]
       );
     }
+  
+  
+  
+      public function store(Request $request)
+      {
+  
+        
+  
+        $schedules =DB::table('schedules')
+        ->insertGetId([
+          'date' =>$request->date,
+          'hour' => $request->hour,
+          'user_id' =>$request->user_id,
+          'observations' =>$request->observations,
+          'status' => 'Finalizado',
+          'created_at'=> Carbon::now(),
+          'updated_at'=>Carbon::now()
+        ]);
+  
+        $services = $request->services;
+  
+        foreach($services as $service){
+          DB::table('services_schedules')
+          ->insert([
+            'service_id' => $service,
+            'schedules_id'=> $schedules,
+            'created_at'=> Carbon::now(),
+            'updated_at'=>Carbon::now()
+          ]);
+        }
+
+        $operator = Auth::id();
+
+        DB::table('attendance')
+          ->insert([
+            'schedules_id'=>$schedules,
+            'operator_id'=> $operator,
+            'is_finished'=>true,
+            'created_at'=> Carbon::now(),
+            'updated_at'=>Carbon::now()
+          ]);
+    
+  
+  
+        return redirect('/attendance')->with('msg', 'Informações gravado com sucesso!');
 }
