ff36ba4fc520ad167bf3c472ac8a556d1f183178
Bruno Pereira <brunoalmeidapereira@gmail.com>
2022-10-26 10:54:15 -0300

    Feat:add validations on services view

--- resources/views/services/create.blade.php
+++ resources/views/services/create.blade.php
@@ -23,19 +23,31 @@
                                     <div class="input-group-prepend">
                                         <span class="input-group-text"> <i class="fas fa-pencil"></i> </span>
                                     </div>
-                                    <input class="form-control" placeholder="Nome" type="text" id="name" name="name">
+                                    <input class="form-control @error('name') is-invalid  @enderror" placeholder="Nome" type="text" id="name" name="name">
+                                    @if ($errors->has('name'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{$errors->first('name')}}
+
+                                        </strong>
+                                    </span>                                    
+                                    @endif
                                 </div>
 
                                 <div class="input-group col-lg-6">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text"> <i class="fas fa-money-bill"></i> </span>
                                     </div>
-                                    <input class="form-control" placeholder="Valor" id="password" name="value">
+                                    <input class="form-control  @error('value') is-invalid  @enderror" placeholder="Valor" id="password" name="value">
+                                    @if ($errors->has('value'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{$errors->first('value')}}
+
+                                        </strong>
+                                    </span>                                    
+                                    @endif
                                 </div>
                             </div>
                             </div>
-
-
                             <div class=" form-group  col-lg-12">
                             <div class="form-row">
                                 <div class="input-group col-lg-6">
@@ -45,7 +57,14 @@
 
                                         <i class="fas fa-pencil-ruler"></i> </span>
                                     </div>
-                                    <textarea class="form-control" id="description" name="description" placeholder="Descrição" style="height:80px"></textarea>
+                                    <textarea class="form-control  @error('description') is-invalid  @enderror" id="description" name="description" placeholder="Descrição" style="height:80px"></textarea>
+                                    @if ($errors->has('description'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{$errors->first('description')}}
+
+                                        </strong>
+                                    </span>                                    
+                                    @endif
                                 </div>
                             </div>
                             </div>
