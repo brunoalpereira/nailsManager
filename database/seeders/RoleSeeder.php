8a6c39dffd416c7cfb77b8b23c7f7431a3dfb054
Bruno Pereira <brunoalmeidapereira@gmail.com>
2023-02-11 12:28:12 -0300

    Feat:create role seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,7 +15,7 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        $role = Role::create(['name' => 'Admin']);
+        $role = Role::create(['name' => 'admin']);
         $role = Role::create(['name' => 'operador']);
         $role = Role::create(['name' => 'usuario']);
     }
