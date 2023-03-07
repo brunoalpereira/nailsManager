b36f9a090e1a5f34ed703a5c41102c50a780e821
Bruno Pereira <brunoalmeidapereira@gmail.com>
2023-03-07 17:01:49 -0300

    Feat: create report controller

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,37 @@
+use PDF;
+
+class ReportController extends Controller
+{
+    public function geraPdf(){
+
+        $pdf =PDF::loadView('pdf');
+
+        return $pdf->setPaper('a4')->stream('serviços.pdf');
+    }
+
+    public function index(){
+        return view('reports.reportsIndex');
+    }
+
+    public function totalServices(){
+
+        $services = DB::table('attendance')
+        ->join('schedules','attendance.schedules_id','schedules.id')
+        ->join('services_schedules','services_schedules.schedules_id','schedules.id')
+        ->join('services','services.id','services_schedules.service_id')
+        ->where('attendance.is_finished',1)
+        ->select('services.name as name',
+            DB::raw('count(services.id) as qtd '))
+         ->groupBy('services.name')
+         ->orderByDesc('qtd')
+         ->get()
+         ->toArray();    
+
+         $teste = Carbon::now();
+         
+            
+
+         $pdf =PDF::loadView('reports.servicesTotalReport',compact('services'));
+
+        return $pdf->setPaper('a4')->stream('serviços.pdf');
+    }
