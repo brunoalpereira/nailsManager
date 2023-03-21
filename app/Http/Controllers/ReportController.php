da5094dc5c6d3b7044376c6626a6ad3cdc9caa71
Bruno Pereira <brunoalmeidapereira@gmail.com>
2023-03-21 17:21:14 -0300

    Feat: create total values services function

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -96,3 +96,32 @@ class ReportController extends Controller
 
         return $pdf->setPaper('a4')->stream('serviços.pdf');
     }
+
+    public function totalValueServices (){
+        $services = DB::table('attendance')
+        ->join('schedules','attendance.schedules_id','schedules.id')
+        ->join('services_schedules','services_schedules.schedules_id','schedules.id')
+        ->join('services','services.id','services_schedules.service_id')
+        ->where('attendance.is_finished',1)
+        ->select('services.name as name',
+            DB::raw('count(services.id) as qtd '),
+                    'services.value as value')
+         ->groupBy('services.name',
+                    'services.value')
+         ->orderByDesc('qtd')
+         ->get()
+         ->toArray();
+         $totalGeral = 0;
+         foreach($services as $service){
+            $total = $service->value * $service->qtd;
+            $service->total = $total;
+
+            $totalGeral+= $service->total;
+         }
+
+
+
+         $pdf =PDF::loadView('reports.servicesTotalValueReport',compact('services','totalGeral'));
+
+        return $pdf->setPaper('a4')->stream('serviços.pdf');
+    }
