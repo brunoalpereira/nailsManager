4562b829f376bc0dc99bc1f2c0233eb9637f6b2a
Bruno Pereira <brunoalmeidapereira@gmail.com>
2023-03-21 17:17:13 -0300

    Feat: create view to total operators report

---
+++ resources/views/reports/servicesTotaByOperatorsReport.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDF</title>
+    <style>
+      .table{
+        font-family: arial,sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+      }
+      .td,.th {
+        border: 0.5px solid #000;
+        text-align: left;
+        padding: 8px;
+      }
+      .th{
+        text-align: center !important;
+      }
+
+      .tr:nth-child(even){
+        background-color: #ddd;
+      }
+      #header {
+        /* position: fixed;
+        left: 0;
+        right: 0; */
+        color: #000;
+        font-size: 0.9em;
+      }
+
+      #header {
+        /* top: 0; */
+        border-bottom: 1px solid #000;
+      }
+
+      .reportTitle {
+        border-top: 1px solid #000;
+        text-align: center;
+        padding: 5px 0px;
+        color: #000;
+        margin-bottom: 30px;
+        font-size: large;
+      }
+
+      #header table {
+        width: 100%;
+        border-collapse: collapse;
+        border: none;
+        margin-top: 5px;
+      }
+
+      #header td{
+        padding: 0;
+        width: 33%;
+      }
+
+    </style>
+      <script src="https://kit.fontawesome.com/ef4e75bc43.js" crossorigin="anonymous"></script>
+</head>
+
+
+<body>
+<div id="header" style="width:100%;">
+      <table style="margin-bottom: 5px;">
+        <tr>
+       
+        <h2 style=" text-align: center;">Nails Manager</h2></td>
+        
+        </tr>
+      </table>
+      
+      <div class="reportTitle">Relatório de total de serviços prestados pelos funcionarios</div>
+    </div>
+
+    <table class="table">
+        <tr class="tr">
+            <th class="th">
+                Funcionario
+            </th>
+            <th class="th">
+                Total
+            </th>
+        </tr>
+        @foreach ($services as $service )
+        <tr class="tr">
+            <td class="td">
+                {{$service->name}}
+            </td>
+            <td class="td">
+                {{$service->qtd}}
+            </td>
+        </tr>
+
+
+
+
+        @endforeach
+    </table>
+
+</body>
+
+</html>
