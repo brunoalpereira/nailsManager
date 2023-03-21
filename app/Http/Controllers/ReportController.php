70e470570a337c8bfff30d9f13a663b7e215ff0b
Bruno Pereira <brunoalmeidapereira@gmail.com>
2023-03-21 17:19:55 -0300

    Feat: create total services month function

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -55,3 +55,23 @@ class ReportController extends Controller
 
         return $pdf->setPaper('a4')->stream('serviços.pdf');
     }
+
+    public function totalServicesMonth(){
+        $today = Carbon::now();
+
+        $services = DB::table('attendance')
+        ->join('schedules','attendance.schedules_id','schedules.id')
+        ->join('services_schedules','services_schedules.schedules_id','schedules.id')
+        ->join('services','services.id','services_schedules.service_id')
+        ->where('attendance.is_finished',1)
+        ->where(DB::raw('month( attendance.created_at)'),$today->month)
+        ->select('services.name as name',
+            DB::raw('count(services.id) as qtd '))
+         ->groupBy('services.name')
+         ->orderByDesc('qtd')
+         ->get()
+         ->toArray();  
+         $pdf =PDF::loadView('reports.servicesTotalMonthReport',compact('services'));
+
+        return $pdf->setPaper('a4')->stream('serviços.pdf');
+    }
